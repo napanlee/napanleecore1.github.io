2fcc464d2a1adbef3e9755d719d2e2931bbcd83b
napanlee <leeg124@newschool.edu>
2022-12-03 18:45:44 -0500

    Commit Update

---
+++ napan_cif22/jquery demo/jquery.js
@@ -0,0 +1,17 @@
+// try playing with this ALERT - 
+$(document).ready(function() {alert("[hello! alert alert drag around and have fun]"); })
+
+// JS files comment with two backslash lines 
+$(document).ready(function() {$("#ufo").draggable(); })
+
+$(document).ready(function() {$("#cat").draggable(); })
+
+$(document).ready(function() {$("#grapes").draggable(); })
+
+$(document).ready(function() {$("#diamond").draggable(); })
+
+$(document).ready(function() {$("#snowman").draggable();})
+
+$(document).ready(function() {$("#chair").draggable();})
+
+$(document).ready(function() {$("#fire").draggable();})
